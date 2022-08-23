26bd0c827b36d09efe80d0d50ec44a4b7e1ce688
DilanTharaka <tharakad049@gmail.com>
2022-08-23 10:33:55 +0530

    solve problem

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express=require('express');
+const mongoose=require('mongoose')
+const app=express()
+const port=4000;
 
-app.get('/', (req, res) => {
-    res.send('Hello Dilan')
+const url='mongodb://localhost/facebook-clone'
+
+const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 5000,
+    autoIndex: false, // Don't build indexes
+    maxPoolSize: 10, // Maintain up to 10 socket connections
+    serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
+    socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+    family: 4 // Use IPv4, skip trying IPv6
+}
+
+mongoose.connect(url, options, {useNewUrlParser: true})
+
+const con=mongoose.connection
+con.on("open",()=>{
+    console.log('MongoDB connected !')
 })
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+app.use(express.json())
+
+app.listen(port,()=>{
+    console.log(`server is started in port ${port}`)
 })
+
